f17d1ec00889cee3077a253b1e2c8119b2f618e1
Ch0coflan <153124045+Ch0coflan@users.noreply.github.com>
2024-11-04 21:46:57 -0500

    Organización del espacio

---
+++ Assets/_scripts/Player/IPlayerContext.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace _scripts.Player
+{
+    public interface IPlayerContext
+    {
+        float GetEnergy();
+        void SetEnergy(float amount);
+        float GetFood();
+        void SetFood();
+    }
+    
+    
+
+   
+
+   
+}
+
