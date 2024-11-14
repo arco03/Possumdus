28016ddfb320ab8777668d166a3423f359c8b962
John Arango Buitrago <153124045+Ch0coflan@users.noreply.github.com>
2024-11-14 15:20:19 -0500

    Sistema de interaccion easter funcional

--- Assets/_objectInteraction/EasterEgg.cs
+++ Assets/_objectInteraction/EasterEgg.cs
@@ -1,47 +1,62 @@
+using System;
 using _scripts.Interfaces;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _objectInteraction
 {
     public class EasterEgg : MonoBehaviour
     {
-        public GameObject easterUI; 
-        public bool toggle; 
+        public GameObject easterUI;
+        public bool toggle;
+        public bool isPlayerInRange;
         public Renderer objectMesh;
-        public GameObject interactablePoint;
+        public GameObject interactableText;
+        public GameObject reticle;
+  
+
         public void OpenCloseEaster()
         {
-            toggle = !toggle;
-            if (!toggle)
+            toggle = !toggle; 
+            if (toggle)
             {
-                easterUI.SetActive(false); 
-                objectMesh.enabled = true; 
-                Debug.Log("Easter Egg closed");
-            }
-            if(toggle)
-            {
-                easterUI.SetActive(true); 
-                objectMesh.enabled = false; 
+                easterUI.SetActive(true);
+                objectMesh.enabled = false;
                 Debug.Log("Easter Egg opened");
-            }
-        }
-
-        private void OnTriggerStay(Collider other)
-        {
-            if(other.CompareTag("Player"))
-            {
-                interactablePoint.SetActive(true);
-                if(Input.GetMouseButtonDown(0))
-                {
-                    
-                    OpenCloseEaster();
-                    
-                }
             }
             else
             {
-                interactablePoint.SetActive(false);
-                
+                easterUI.SetActive(false);
+                objectMesh.enabled = true;
+                Debug.Log("Easter Egg closed");
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                isPlayerInRange = true;
+                interactableText.SetActive(true);
+                reticle.SetActive(false);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                isPlayerInRange = false;
+                interactableText.SetActive(false);
+                reticle.SetActive(true);
+            }
+        }
+
+        private void Update()
+        {
+            if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
+            {
+                OpenCloseEaster(); 
             }
         }
     }
