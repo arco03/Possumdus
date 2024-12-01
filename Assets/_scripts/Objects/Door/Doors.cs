0d387a22b128e1af0081d920b0895b8e1b61197b
arco03 <dmglux13@gmail.com>
2024-12-01 18:28:54 -0500

    Update: Cambios puertas

--- Assets/_scripts/Objects/Door/Doors.cs
+++ Assets/_scripts/Objects/Door/Doors.cs
@@ -1,23 +1,14 @@
-using System;
 using UnityEngine;
 
 namespace _scripts.Objects.Door
 {
     public class Doors : MonoBehaviour
     {
-        [SerializeField] private Animator animator;
+        [SerializeField] public Animator animator;
         [SerializeField] private string animationName;
-        private void Awake()
-        {
-            animator.GetComponent<Animator>();
-        }
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.CompareTag("Player"))
-            {
-                Debug.Log("Entro al animator puerta");
-                animator.Play(animationName);
-            }
+        public void AnimationDoor()
+        {  
+            animator.SetTrigger(animationName);
         }
     }
 }
